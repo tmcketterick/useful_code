9e6cfe9b41e40b67281b5a6ca6bb63a42c6dbdc4
Tom <thomas.mcketterick@mindfoundry.ai>
2021-04-01 16:51:44 +0100

    Simple FastApi example added to Python utils

---
+++ python/fastapi/main.py
@@ -0,0 +1,19 @@
+# https://fastapi.tiangolo.com/tutorial/first-steps/
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World, this is Tom!"}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int=0, limit: int=10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
