ec5bbd5ffc6e09a16941e1e6c5471cbdc4f0a9d5
Tom <thomas.mcketterick@mindfoundry.ai>
2021-04-06 14:57:07 +0100

    Adding dockerfile to fastapi example

--- python/fastapi/app/main.py
+++ python/fastapi/app/main.py
@@ -13,7 +13,6 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 async def read_item(skip: int=0, limit: int=10):
     return fake_items_db[skip : skip + limit]
 
-
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
